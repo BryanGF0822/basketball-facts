97ec3d4241cf4d099bd2a4953c6dbec8fdf5d955
BryanGF0822 <bryanicesi0822@gmail.com>
2021-04-16 12:07:06 -0500

    The player class is created with all its characteristics.

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,5 +1,92 @@
 package model;
 
 public class Player {
+	
+	private String nombre;
+	private int edad;
+	private String equipo;
+	private int puntosPorPartido;
+	private int rebotesPorPartido;
+	private int asistenciasPorPartido;
+	private int robosPorPartido;
+	private int bloqueosPorPartido;
+	
+	public Player(String nombre, int edad, String equipo, int puntosPorPartido, int rebotesPorPartido,
+			int asistenciasPorPartido, int robosPorPartido, int bloqueosPorPartido) {
+		
+		this.nombre = nombre;
+		this.edad = edad;
+		this.equipo = equipo;
+		this.puntosPorPartido = puntosPorPartido;
+		this.rebotesPorPartido = rebotesPorPartido;
+		this.asistenciasPorPartido = asistenciasPorPartido;
+		this.robosPorPartido = robosPorPartido;
+		this.bloqueosPorPartido = bloqueosPorPartido;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getEquipo() {
+		return equipo;
+	}
+
+	public void setEquipo(String equipo) {
+		this.equipo = equipo;
+	}
+
+	public int getPuntosPorPartido() {
+		return puntosPorPartido;
+	}
+
+	public void setPuntosPorPartido(int puntosPorPartido) {
+		this.puntosPorPartido = puntosPorPartido;
+	}
+
+	public int getRebotesPorPartido() {
+		return rebotesPorPartido;
+	}
+
+	public void setRebotesPorPartido(int rebotesPorPartido) {
+		this.rebotesPorPartido = rebotesPorPartido;
+	}
+
+	public int getAsistenciasPorPartido() {
+		return asistenciasPorPartido;
+	}
+
+	public void setAsistenciasPorPartido(int asistenciasPorPartido) {
+		this.asistenciasPorPartido = asistenciasPorPartido;
+	}
+
+	public int getRobosPorPartido() {
+		return robosPorPartido;
+	}
+
+	public void setRobosPorPartido(int robosPorPartido) {
+		this.robosPorPartido = robosPorPartido;
+	}
+
+	public int getBloqueosPorPartido() {
+		return bloqueosPorPartido;
+	}
+
+	public void setBloqueosPorPartido(int bloqueosPorPartido) {
+		this.bloqueosPorPartido = bloqueosPorPartido;
+	}
+	
+	
 }
