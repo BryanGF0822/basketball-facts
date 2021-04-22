95803f4aa3ecc3415b34221d92d9ba7b47d02722
johnkennedyls <jkls1998@gmail.com>
2021-04-21 22:22:32 -0500

    OpenCsV creado

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,27 @@
 package ui;
 
+import java.io.FileReader;
+import java.util.Arrays;
+
+import com.opencsv.CSVReader;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		try {
+			CSVReader reader = new CSVReader(new FileReader("/data/PlayersList.csv"));
+			String[] nextLine;
+			while((nextLine = reader.readNext()) != null) {
+				if(nextLine != null) {
+					System.out.println(Arrays.toString(nextLine));
+				}
+			}
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		
+		System.out.println("Lo lei ");
 	}
 
 }
