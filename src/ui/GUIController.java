f50c65ff0e1f39eb4fb316dc168a973ae592f8be
SeanQO <seanquintero3@gmail.com>
2021-04-23 19:53:12 -0500

    agregado ventana para seleccionar rangos por numeros y por nombre

--- src/ui/GUIController.java
+++ src/ui/GUIController.java
@@ -2,10 +2,32 @@ package ui;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import model.BasketballAgency;
 
 public class GUIController {
 	
+	// ********** search name window **********
+	@FXML
+    private Label filterByNameLabel;
+
+    @FXML
+    private TextField nameToSearch;
+    
+    // ********** search range window **********
+    @FXML
+    private Label filterByRangeLabel;
+
+    @FXML
+    private TextField minRange;
+
+    @FXML
+    private TextField maxRange;
+    
+    // ********** model atributes **********
 	private static BasketballAgency ba ;
 	
 	public GUIController() throws IOException {
@@ -14,5 +36,18 @@ public class GUIController {
 		ba.importPlayers("data/players.csv");
 		
 	}
+	
+	// ********** search name window action **********
+    @FXML
+    void searchByName(ActionEvent event) {
+
+    }
+    
+    // ********** search range action **********
+    @FXML
+    void searchByRange(ActionEvent event) {
+
+    }
+	
 	
 }
