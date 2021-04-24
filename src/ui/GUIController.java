ea00bbcc93f07e9b6b7b425a8e6bb3cd49027a13
SeanQO <seanquintero3@gmail.com>
2021-04-24 02:14:29 -0500

    linked main windos buttons with filter windows

--- src/ui/GUIController.java
+++ src/ui/GUIController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import model.BasketballAgency;
 
 public class GUIController {
@@ -41,11 +42,13 @@ public class GUIController {
 	// ********** import csv path **********
 	private final String CSV_PATH =  "data/players.csv";
 	
+	// ********** controller atributes **********
+	private boolean isSearching;
+	
 	public GUIController() {
 		ba = new BasketballAgency();
+		isSearching = false;
 	}
-	
-	
 	
 	// ********** load fxml **********
 	
@@ -58,6 +61,7 @@ public class GUIController {
 			Stage stage = new Stage();
 			stage.setScene(scene);
 			stage.setTitle("Basketball Facts!");
+			stage.setResizable(false);
 			stage.show();
 
 		} catch (IOException ioException) {
@@ -65,67 +69,180 @@ public class GUIController {
 		} 
 	}
 	
-	@FXML
-    void load(ActionEvent event) {
+	
+	private void LoadSearchByNameWindow(String nameToDisplay) {
 		try {
-			ba.importPlayers(CSV_PATH);
-			ba.addPlayersStatics();
+			FXMLLoader fxmll = new FXMLLoader(getClass().getResource("fxml/nombre.fxml"));
+			fxmll.setController(this);
+			Parent root = fxmll.load();
+			Scene scene = new Scene(root);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.setTitle("Basketball Facts!");
+			stage.setResizable(false);
+			stage.setAlwaysOnTop(true);
+			stage.initStyle(StageStyle.UTILITY);
+			stage.show();
+			stage.setOnCloseRequest(e -> closeSearchStage() );
 			
-			//Closes logo loading window.
-			Stage logoStage = (Stage) logoAP.getScene().getWindow();
-		    logoStage.close();
+			isSearching = true;
+			filterByNameLabel.setText(nameToDisplay);
 			
-			LoadMainWindow();
-		    
+			
 		} catch (IOException ioException) {
 			// TODO: handle exception with an alert that displays the content of the error.
 		} 
-    }
-
+		
+	}
+	
+	private void LoadSearchByRangeWindow(String nameToDisplay) {
+		try {
+			FXMLLoader fxmll = new FXMLLoader(getClass().getResource("fxml/rango.fxml"));
+			fxmll.setController(this);
+			Parent root = fxmll.load();
+			Scene scene = new Scene(root);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.setTitle("Basketball Facts!");
+			stage.setResizable(false);
+			stage.setAlwaysOnTop(true);
+			stage.initStyle(StageStyle.UTILITY);
+			stage.show();
+			stage.setOnCloseRequest(e -> closeSearchStage() );
+			
+			isSearching = true;
+			filterByRangeLabel.setText(nameToDisplay);
+			
+			
+		} catch (IOException ioException) {
+			// TODO: handle exception with an alert that displays the content of the error.
+		} 
+		
+	}
+	
+	// ********** logo Window Action **********
+	@FXML
+	void load(ActionEvent event) {
+		try {
+			ba.importPlayers(CSV_PATH);
+			ba.addPlayersStatics();
+				
+			//Closes logo loading window.
+			Stage logoStage = (Stage) logoAP.getScene().getWindow();
+			logoStage.close();
+				
+			LoadMainWindow();
+			    
+		} catch (IOException ioException) {
+				// TODO: handle exception with an alert that displays the content of the error.
+		}
+		
+		
+	}
+	
+	
+	 private void closeSearchStage() {
+		 isSearching = false;
+	}
+	 
     // ********** Principal Windows Actions **********
     @FXML
     void filterByAge(ActionEvent event) {
-
+    	if (!isSearching) {
+    		LoadSearchByRangeWindow("Edad");
+    		
+		}else {
+			//TODO
+		}
+    	
     }
 
     @FXML
     void filterByAssistance(ActionEvent event) {
-
+    	if (!isSearching) {
+    		LoadSearchByRangeWindow("asistencias");
+    		
+		}else {
+			//TODO
+		}
+    	
     }
 
     @FXML
     void filterByBlocks(ActionEvent event) {
-
+    	if (!isSearching) {
+    		LoadSearchByRangeWindow("Bloqueos");
+    		
+		}else {
+			//TODO
+		}
+    	
     }
 
     @FXML
     void filterByFouls(ActionEvent event) {
-
+    	if (!isSearching) {
+    		LoadSearchByRangeWindow("Faltas");
+    		
+		}else {
+			//TODO
+		}
+    	
     }
 
     @FXML
     void filterByName(ActionEvent event) {
-
+    	if (!isSearching) {
+    		LoadSearchByNameWindow("Nombre del jugador");
+    		
+		}else {
+			//TODO
+		}
+    	
     }
 
     @FXML
     void filterByRebounds(ActionEvent event) {
-
+    	if (!isSearching) {
+    		LoadSearchByRangeWindow("Rebotes");
+    		
+		}else {
+			//TODO
+		}
+    	
     }
 
     @FXML
-    void filterByScore(ActionEvent event) {
-
+    void filterByScore(ActionEvent event) {	
+    	if (!isSearching) {
+    		LoadSearchByRangeWindow("Puntos");
+    		
+		}else {
+			//TODO
+		}
+    	
     }
 
     @FXML
     void filterBySteal(ActionEvent event) {
-
+    	if (!isSearching) {
+    		LoadSearchByRangeWindow("Robos");
+    		
+		}else {
+			//TODO
+		}
+    	
     }
 
     @FXML
     void filterByTeam(ActionEvent event) {
-
+    	if (!isSearching) {
+    		LoadSearchByNameWindow("Nombre del Equipo");
+    		
+		}else {
+			//TODO
+		}
+    	
     }
 	
 	// ********** search name window action **********
@@ -142,3 +259,4 @@ public class GUIController {
 	
 	
 }
+
