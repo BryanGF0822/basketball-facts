863f1f021824814cac97edc93aef7f9be7c45913
BryanGF0822 <bryanicesi0822@gmail.com>
2021-04-25 00:20:41 -0500

    AVL interface error correction.

--- src/collections/BalancedBSTree.java
+++ src/collections/BalancedBSTree.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public interface BalancedBSTree<K extends Comparable<K>, V> {
 	public Node<K, V>add(K key,V value);
-	public List<V> search(K key);
 	public void delete(K key);
 	public boolean contains(K key);
 	
